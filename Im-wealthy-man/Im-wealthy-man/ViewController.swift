ef41f616dc74a6c3314183cc73aaa0314138cf4a
victor.agbeo <vicagbeo@outlook.com>
2023-05-25 10:57:04 -0500

    Initial Commit

---
+++ Im-wealthy-man/Im-wealthy-man/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Im-wealthy-man
+//
+//  Created by vicagbeo on 6/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
