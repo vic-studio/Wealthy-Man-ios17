5c56788568f54d2df82e22a8cd920eabc64ce7ce
victor.agbeo <130013310+victor-aa1@users.noreply.github.com>
2023-06-14 20:04:31 -0500

    Add files via upload

---
+++ Wealthy-Man-ios17/Im-wealthy-man/Im-wealthy-manUITests/Im_wealthy_manUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Im_wealthy_manUITestsLaunchTests.swift
+//  Im-wealthy-manUITests
+//
+//  Created by vicagbeo on 6/14/23.
+//
+
+import XCTest
+
+final class Im_wealthy_manUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
